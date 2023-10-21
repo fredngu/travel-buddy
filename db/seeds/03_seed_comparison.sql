454c898a3ca0a47655c4fe548b60f77e1012efd5
Pablo Picasso <paul.mca.ca@gmail.com>
2023-10-21 15:49:47 -0400

    Update db names

--- db/seeds/03_seed_comparison.sql
+++ db/seeds/03_seed_comparison.sql
@@ -1,3 +1,3 @@
-INSERT INTO comparison (comparison_id, user_id, trip1, trip2, trip3, trip4, name)
-VALUES
+INSERT INTO comparison (comparison_id, traveller_id, trip1, trip2, trip3, trip4, name)
+VAS
 (1, 20, 20, 21, 22, 23, 24, "Scandanavian Comparison")
