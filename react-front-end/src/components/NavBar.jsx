f4a7d6b1bb57cc04c3be1044650b56d85a57975a
Glauber Marques <glauberfmarques@gmail.com>
2023-10-27 07:46:57 -0600

    Styling Navbar component

--- react-front-end/src/components/NavBar.jsx
+++ react-front-end/src/components/NavBar.jsx
@@ -1,34 +1,39 @@
-import React from "react";
-import "../styles/NavBar.scss";
-
-import { Auth0Provider } from "@auth0/auth0-react";
-import LoginButton from "../api/login";
-import LogoutButton from "../api/logout";
-import Profile from "../api/profile";
+import React from 'react';
+import { AppBar, Toolbar, Button, Typography, Container } from '@mui/material';
+import { Auth0Provider } from '@auth0/auth0-react';
+import LoginButton from '../api/login';
+import LogoutButton from '../api/logout';
+import Profile from '../api/profile';
+import '../styles/NavBar.scss';
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <ul>
-        <Auth0Provider
-          domain="dev-6jhms23po3hoo2lj.us.auth0.com"
-          clientId="TybkdIkTe6Yd8FMUl78uDXEy6dREGsry"
-          authorizationParams={{
-            redirect_uri: window.location.origin,
-          }}
-        >
-          <li className="nav-item">
-            <LoginButton />
-          </li>
-          <li className="nav-item">
-            <LogoutButton />
-          </li>
-          <li className="nav-item">
-            <Profile />
-          </li>
-        </Auth0Provider>
-      </ul>
-    </nav>
+    <AppBar position="static">
+      <Container>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Travel Buddy
+          </Typography>
+          <Auth0Provider
+            domain="dev-6jhms23po3hoo2lj.us.auth0.com"
+            clientId="TybkdIkTe6Yd8FMUl78uDXEy6dREGsry"
+            authorizationParams={{
+              redirect_uri: window.location.origin,
+            }}
+          >
+            <Button color="inherit">
+              <LoginButton />
+            </Button>
+            <Button color="inherit">
+              <LogoutButton />
+            </Button>
+            <Button color="inherit">
+              <Profile />
+            </Button>
+          </Auth0Provider>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
