012999efac67bbf7eb4f55f08d0cdc9e39ee4713
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-10-31 21:49:44 -0400

    Merge trips

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,3 +1,8 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import Flight from './components/flights//FlightSearch';
+import Hotel from './components/hotels/HoteSearchPage';
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
@@ -12,33 +17,10 @@ import { Hotel } from "./pages/Hotel";
 import { TripSummary } from "./pages/TripSummary";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Click the button to load data!",
-    };
-  }
-
-  fetchData = () => {
-    axios.get("/api/data").then((response) => {
-      // handle success
-      console.log(response.data);
-
-      console.log(response.data.message);
-      this.setState({
-        message: response.data.message,
-      });
-    });
-  };
-
   render() {
     return (
       <div className="App">
         <NavBar />
-        <h1>{this.state.message}</h1>
-        <Button variant="contained" onClick={this.fetchData}>
-          Fetch Data
-        </Button>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/trips" element={<MyTrips />} />
