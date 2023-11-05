e48ba903ac8e1bc782f44156d74fc495adf6375a
glaubervilane <glauberfmarques@gmail.com>
2023-11-05 10:54:54 -0700

    Fix a lint warning

--- react-front-end/src/pages/TripSummary.jsx
+++ react-front-end/src/pages/TripSummary.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import FlightSummary from "../components/FlightSummary";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useItineraryData } from "../components/utils/ItineraryDataContext";
